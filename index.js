7767451c46b2ea306d92134a84f3071e81fe7065
Abhishek Tiwari <abhishek.tiwari17338@marwadieducation.edu.in>
2022-05-21 16:00:23 +0530

    features: update

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('hello')
+
+var btn = document.getElementById('btn');
+var exp = document.getElementById('explain');
+
+
+    btn.addEventListener('click',()=>{
+    exp.style.display = 'block';
+    btn.style.display = 'none';
+})
+
+
+
